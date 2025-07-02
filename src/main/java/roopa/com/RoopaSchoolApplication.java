0dad473e856c0a56e0d953fe366b20ed24210653
akkhil97 <asrch25@gmail.com>
2025-07-02 11:35:41 +0530

    changes in notices section

--- src/main/java/roopa/com/RoopaSchoolApplication.java
+++ src/main/java/roopa/com/RoopaSchoolApplication.java
@@ -2,19 +2,16 @@ package roopa.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-
 public class RoopaSchoolApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RoopaSchoolApplication.class, args);
 	}
 
-	// Inner static configuration class
 	@Configuration
 	public static class WebConfig implements WebMvcConfigurer {
 		@Override
@@ -26,4 +23,3 @@ public class RoopaSchoolApplication {
 		}
 	}
 }
-
